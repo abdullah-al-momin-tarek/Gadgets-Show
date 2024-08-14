22e1a95e3b82f3df3c7b250664b1232bd87b10f5
Shikder Tarek <shikdertarek2019@gmail.com>
2024-08-15 01:33:32 +0600

    Add private Route

---
+++ src/Components/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+import { Navigate } from "react-router-dom";
+
+const PrivateRoute = ({children}) => {
+    const {user, loader} = useContext(AuthContext)
+
+    if(loader){
+        return <span className="loading loading-spinner loading-lg"></span>
+    }
+
+    if(user){
+        return children
+    }
+    return (
+        <Navigate to={'/login'}></Navigate>
+    );
+};
+
+export default PrivateRoute;
