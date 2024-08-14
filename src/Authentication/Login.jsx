961bde541cb4efbf6d844bcf096501c6c03c5c1b
Shikder Tarek <shikdertarek2019@gmail.com>
2024-08-14 23:33:02 +0600

    add nav

--- src/Authentication/Login.jsx
+++ src/Authentication/Login.jsx
@@ -1,7 +1,7 @@
 
 const Login = () => {
     return (
-        <section className="flex items-center justify-center">
+        <section className="flex items-center justify-center mt-12">
             <div className="w-full max-w-md p-4 rounded-md shadow sm:p-8 dark:bg-gray-50 dark:text-gray-800 ">
 	<h2 className="mb-3 text-3xl font-semibold text-center">Login to your account</h2>
 	<p className="text-sm text-center dark:text-gray-600">Dont have account?
--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar/Navbar";
 
 const Root = () => {
     return (
-        <div className="poppins">
+        <div className="poppins container mx-auto">
+            <Navbar/>
             <Outlet/>
         </div>
     );
